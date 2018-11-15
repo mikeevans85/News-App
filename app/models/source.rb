e96715140cfda5229c2abd939d7c8e5c5f77744d
mikeevans85 <baudecordier@gmail.com>
2018-11-14 21:14:36 -0600

    Added model relations and routes

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,2 +1,3 @@
 class Source < ApplicationRecord
+  has_many :user_sources
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+
+    post "/users" => "users#create"
+
+    get "/sources" => "sources#index"
+    post "/sources" => "usersources#create"
+    get "/sources :id" => "sources#show"
+    delete "/sources :id" => "usersources#destroy"
+
+    get "/feed" => "usersources#index"
+
+  end
 end
